af001438f85d799bbe7a3356d30e9ea57851fb39
YatesyTea <danyates1997@gmail.com>
2021-08-04 11:45:54 +0100

    cleanup of app.py

--- recipeApp/app.py
+++ recipeApp/app.py
@@ -9,6 +9,8 @@ from wtforms.validators import DataRequired
 app = Flask(__name__, template_folder="./templates")
 app.config['SECRET_KEY'] = "secret_key"
 
+recipe_details = {}
+
 # form for user to input their preferences 
 class DietaryForm(FlaskForm):
     fat = IntegerField("How many grams of fat would you like to consume per meal?", validators=[DataRequired()])
@@ -16,7 +18,7 @@ class DietaryForm(FlaskForm):
     protein = IntegerField("How much protein would to consume per meal?", validators=[DataRequired()])
     submit = SubmitField("Submit")
     
-
+# Setting http methods, where the requests are going.
 @app.route("/", methods=["GET", "POST"])
 def home():
     form = DietaryForm(request.form)
@@ -33,18 +35,25 @@ def home():
 
         message = "Thank you. You will now be redirected to your bespoke recipe recommendations" 
         
-        return redirect(url_for(f"recipes", fat=fat, calories=calories, protein=protein))
+        return redirect(url_for("recipes", fat=fat, calories=calories, protein=protein))
     
     return render_template("index.html", form=form, message=message)
 
-@app.route("/recipes/<fat>_<calories>_<protein>")
+@app.route("/recipes/<fat>_<calories>_<protein>/")
 def recipes(fat, calories, protein):
-    # error exception here to redirect them to homepage if they enter nothing
+    # error exception here to redirect them to homepage if they enter nothing 
     recipes = random.choices(list(main.find_recipes(fat,calories,protein)), k=3)
-    return render_template("recipes.html", recipes=recipes)
+    return render_template("recipes.html", recipes=recipes) 
 
-@app.route("/instructions")
-def instructions(recipe):
-    return render_template("instructions.html", recipe=recipe)
+
+@app.route("/recipes/<fat>_<calories>_<protein>/<recipe_id>") 
+def instructions(fat, calories, protein, recipe_id): 
+    recipes = main.find_recipes(fat,calories,protein)
+    recipes.rewind()
+
+    for recipe in recipes:
+        if str(recipe["_id"]) == str(recipe_id):
+            return render_template("instructions.html", recipe=recipe)
+
 if __name__ == "__main__":
     app.run(port=5000, debug=True) 
